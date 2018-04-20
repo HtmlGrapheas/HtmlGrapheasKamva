1985811b9fa4576a5f0c5b1706b8b403589aefbe
NikitaFeodonit <nfeodonit@yandex.com>
2018-04-20 11:22:36 +0300

    Replace 'typedef' to 'using' in PixelFormatConvertor.

--- src/hgkamva/platform/wxwidgets/PixelFormatConvertor.h
+++ src/hgkamva/platform/wxwidgets/PixelFormatConvertor.h
@@ -53,70 +53,70 @@ struct wxWidgetsToAGGHelper
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 24, 0, 1, 2, -1>
 {
-  typedef agg::pixfmt_rgb24 format;
+  using format = agg::pixfmt_rgb24;
 };
 
 /// 24-bit BGR
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 24, 2, 1, 0, -1>
 {
-  typedef agg::pixfmt_bgr24 format;
+  using format = agg::pixfmt_bgr24;
 };
 
 /// 32-bit RGB, alpha unused but stored as ARGB.
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 32, 1, 2, 3, -1>
 {
-  typedef agg::pixfmt_argb32 format;
+  using format = agg::pixfmt_argb32;
 };
 
 /// 32-bit RGB, alpha unused but stored as RGBA.
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 32, 0, 1, 2, -1>
 {
-  typedef agg::pixfmt_rgba32 format;
+  using format = agg::pixfmt_rgba32;
 };
 
 /// 32-bit BGR, alpha unused but stored as ABGR.
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 32, 3, 2, 1, -1>
 {
-  typedef agg::pixfmt_abgr32 format;
+  using format = agg::pixfmt_abgr32;
 };
 
 /// 32-bit BGR, alpha unused but stored as BGRA.
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 32, 2, 1, 0, -1>
 {
-  typedef agg::pixfmt_bgra32 format;
+  using format = agg::pixfmt_bgra32;
 };
 
 /// 32-bit RGBA
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 32, 0, 1, 2, 3>
 {
-  typedef agg::pixfmt_rgba32 format;
+  using format = agg::pixfmt_rgba32;
 };
 
 /// 32-bit BGRA
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 32, 2, 1, 0, 3>
 {
-  typedef agg::pixfmt_bgra32 format;
+  using format = agg::pixfmt_bgra32;
 };
 
 /// 32-bit ARGB
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 32, 1, 2, 3, 0>
 {
-  typedef agg::pixfmt_argb32 format;
+  using format = agg::pixfmt_argb32;
 };
 
 /// 32-bit ABGR
 template <>
 struct wxWidgetsToAGGHelper<unsigned char, 32, 3, 2, 1, 0>
 {
-  typedef agg::pixfmt_abgr32 format;
+  using format = agg::pixfmt_abgr32;
 };
 
 }  // namespace
@@ -132,16 +132,16 @@ template <typename wxWidgetsPixelFormat>
 class PixelFormatConvertor
 {
 public:
-  typedef wxWidgetsPixelFormat wxWidgetsType;
+  using wxWidgetsType = wxWidgetsPixelFormat;
 
   // Break out the wxWidgets parameters and feed to the helper class.
-  typedef
+  using AGGType =
       typename wxWidgetsToAGGHelper<typename wxWidgetsPixelFormat::ChannelType,
           wxWidgetsPixelFormat::BitsPerPixel,
           wxWidgetsPixelFormat::RED,
           wxWidgetsPixelFormat::GREEN,
           wxWidgetsPixelFormat::BLUE,
-          wxWidgetsPixelFormat::ALPHA>::format AGGType;
+          wxWidgetsPixelFormat::ALPHA>::format;
 };  // class PixelFormatConvertor
 
 }  // namespace hg
